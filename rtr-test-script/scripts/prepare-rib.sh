38b0a58adbbaf0b2420f1606b582e6a2fdc29ae8
Colin Sames <sames.colin@gmail.com>
2017-02-28 15:35:43 +0100

    Update libraries.

--- rtr-test-script/scripts/prepare-rib.sh
+++ rtr-test-script/scripts/prepare-rib.sh
@@ -10,5 +10,5 @@ printf " done!\n"
 
 #take only ASN and Prefix and write them to different files
 printf "Filter unnecessary data, sort and uniq it..."
-awk -F '|' '{print $6 " " $7}' ../tmp/$RIB_DUMP.temp | awk '{ print $1 " " $NF }' | sed '/{/d' | sort -u -k 1 > ../tmp/$RIB_DUMP.formatted
+awk -F '|' '{print $6 " " $7}' ../tmp/$RIB_DUMP.temp | sed 's/{\|}//g' | awk '{ n=split($NF, a, ","); print $1 " " a[n] }' | sort -u -k 1 > ../tmp/$RIB_DUMP.formatted
 printf " done!\n"
